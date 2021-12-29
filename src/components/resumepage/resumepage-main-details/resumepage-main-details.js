98dd61450199a9141580c288afe9e062da5b574d
rohit katbamna <rohitkatbamna@protonmail.com>
2021-12-29 10:06:50 +0530

    second commit

--- src/components/resumepage/resumepage-main-details/resumepage-main-details.js
+++ src/components/resumepage/resumepage-main-details/resumepage-main-details.js
@@ -9,13 +9,12 @@ function Resumepagemaindetails(){
             <div className="row">
                 <div className="col-12">
                     <div class="card" style={{ borderRadius:'16px',borderTop:'10px solid white'}}>
-                        <div class="card-body resume-page-main-details-typical">
+                        <div class="card-body resume-page-main-details-typical text-end">
                             <Typical
-                                steps={['C:Users&#x8;rohit>', 1000, 'C:\Users\rohit> Hello There. You are at rohitkatbamna.com!!', 1000]}
+                                steps={["'C:/Users/rohit>'", 1000, 'C:/Users/rohit> Hello There. You are at rohitkatbamna.com!!', 1000]}
                                 loop={1}
                                 wrapper="p"
                             />
-                            <h1>Hi </h1>
                         </div>
                     </div>
                 </div>
