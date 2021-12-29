6875644b00207741f97387fcc90f59872b1a98d6
rohit katbamna <rohitkatbamna@protonmail.com>
2021-12-29 10:34:11 +0530

    fifth commit

--- src/components/resumepage/resumepage-main-details/resumepage-main-details.js
+++ src/components/resumepage/resumepage-main-details/resumepage-main-details.js
@@ -9,12 +9,24 @@ function Resumepagemaindetails(){
             <div className="row">
                 <div className="col-12">
                     <div class="card" style={{ borderRadius:'16px',borderTop:'10px solid white'}}>
-                        <div class="card-body resume-page-main-details-typical text-end">
-                            <Typical
-                                steps={['/C/Users/rohit>', 1000, '/C/Users/rohit> Hello There. You are at rohitkatbamna.com!!', 1000]}
-                                loop={1}
-                                wrapper="p"
-                            />
+                        <div class="card-body resume-page-main-details-typical">
+                            <div className="text-end">
+                                <Typical
+                                    steps={['/C/Users/rohit>', 1000, '/C/Users/rohit> Hello There. You are at rohitkatbamna.com!!', 1000]}
+                                    loop={1}
+                                    wrapper="p"
+                                />
+                            </div>
+                            <div className="text-right resume-page-main-details-hi-rohit">
+                                <p>Hi I am Rohit Katbamna</p>
+                            </div>
+                            <div className="text-right resume-page-main-details-i-am">
+                                <Typical
+                                    steps={['I am', 1000, ' I am web developer', 1000 , ' I am react developer', 1000, ' I am HTML developer', 1000 , ' I am font end developer', 1000 , ' I am python developer', 1000]}
+                                    loop={Infinity}
+                                    wrapper="p"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
