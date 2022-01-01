43c0028703d09ae0163293db08c60c542d858681
rohit katbamna <rohitkatbamna@protonmail.com>
2022-01-01 09:25:39 +0530

    1 jan 2nd commit

--- src/components/resumepage/resumepage-main-details/resumepage-main-details.js
+++ src/components/resumepage/resumepage-main-details/resumepage-main-details.js
@@ -14,9 +14,9 @@ function Resumepagemaindetails(){
                 <div className="col-12">
                     <div class="card" style={{ borderRadius:'16px',borderTop:'10px solid white', borderBottom:'10px solid white'}}>
                         <div class="card-body resume-page-main-details-typical">
-                            <div className="text-center resume-page-main-details-hi-rohit">
+                            <div className="text-center resume-page-main-details-hi-rohit fw-bold">
                                 <Typical
-                                    steps={['hello world', 3000, 'ハローワールド', 3000 , 'नमस्ते दुनिया', 3000, 'કેમ છો દુનિયા', 3000 , '전 세계 여러분 안녕하세요', 3000 ,' Hej världen',3000, 
+                                    steps={['Hello World', 3000, 'ハローワールド', 3000 , 'नमस्ते दुनिया', 3000, 'કેમ છો દુનિયા', 3000 , '전 세계 여러분 안녕하세요', 3000 ,' Hej världen',3000, 
                                     'Salut tout le monde', 3000 ,'হ্যালো ওয়ার্ল্ড', 3000 ,'Всем привет', 3000,'Γειά σου Κόσμε', 3000, 'Rytsas vys',3000, 'ሰላም ልዑል', 3000]}
                                     loop={Infinity}
                                     wrapper="p"
@@ -25,15 +25,15 @@ function Resumepagemaindetails(){
                             <div className="text-right resume-page-main-details-i-am text-center">
                                 <h1>I develop react based websites</h1> 
                             </div>
-                            <div className="text-center resume-page-main-details-summary-what-i-am mt-5">
-                                <h2>Something to say about myself</h2>
+                            <div className="text-center resume-page-main-details-summary-what-i-am mt-2">
+                                <h2 className="fw-bold">Something to say about myself</h2>
                                 <h3>Front End Developer with experience in blending art of design with skill 
                                     of programming to deliver engaging user experience through efficient website 
                                     development, proactive feature optimization, and relentless debugging.
                                 </h3>
                             </div>
                             <div className="resume-page-main-details-what-skills-i-have text-center">
-                                <p className="fs-1 ms-4">Skills I have</p>
+                                <p className="fs-2 ms-4 fw-bold">Skills I have</p>
                                 <ul className="resume-page-main-details-what-skills-i-have-list">
                                     <li><FontAwesomeIcon icon={faHtml5}/> HTML</li>
                                     <li><FontAwesomeIcon icon={faCss3Alt} /> CSS</li>
