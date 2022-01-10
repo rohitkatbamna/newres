dd22e6b2e615b3e04de5f62a15409897af69e734
rohit katbamna <rohitkatbamna@protonmail.com>
2022-01-10 10:58:11 +0530

    10th jan 3rd commit

--- src/components/projectdetailspage/project-news-firstproject/project-news-firstproject.js
+++ src/components/projectdetailspage/project-news-firstproject/project-news-firstproject.js
@@ -8,15 +8,9 @@ import './project-news-firstproject.css';
 function Newsprojectfirst(){
     const [news, setNews] = useState([]);
 
-    function newsFunction(){
+    function newsFunction(event){
         const apikey = 'tiyqI1hdLsc1ID9LMbzyE3I68r7gRC4o';
-        fetch(`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${apikey}`)
-        .then(response => response.json())
-        .then(data => {
-            setNews(data.results);
-            console.log(news[0].multimedia[0].url);
-            }
-        );
+        console.log(event.target.attributes.value.nodeValue)
     }
     return(
         <>
@@ -29,25 +23,11 @@ function Newsprojectfirst(){
                     <div className="row text-center">
                         <div className="col-12">
                             <h1>Click button below To fetch Top Stories from world</h1>
-                            <button type="button" className="btn btn-dark" onClick={newsFunction}>Click Me Now</button>
+                            <button type="button" className="btn btn-dark" onClick={newsFunction} value="world">World</button>
                         </div>
                     </div>
-                    <div className="row text-right mt-5">
-                        <div className="col-12">
-                            {news.map(newsdata =>
-                                {
-                                return(
-                                    <div className="card mt-5 shadow" style={{ borderRadius:'16px'}}>
-                                        <div className="card-body">
-                                            <h2 className="fw-bold">{newsdata.title}</h2>
-                                            <h3>{newsdata.abstract}</h3>
-                                        </div>
-                                    </div>
-                                );
-                                })
-                            }
-                        </div>
-                    </div>
+
+
                 </div>        
             </div>
         </div>
