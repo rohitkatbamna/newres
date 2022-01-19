001aebcf5024a4fda0047f9b3db5d1cc6406e651
rohit katbamna <rohitkatbamna@protonmail.com>
2022-01-19 10:14:51 +0530

    19 jan 5th commit

--- src/components/resumepage/resumepage-navbar/resumepage-navbar.js
+++ src/components/resumepage/resumepage-navbar/resumepage-navbar.js
@@ -8,12 +8,12 @@ function Resumepagenavbar(){
     return(
         <nav className="navbar navbar-expand-lg  navbar-dark resume-page-navbar-everything sticky-top">
             <div className="container-fluid">
-                <a className="navbar-brand resume-page-navbar-brand">
+                <div className="navbar-brand resume-page-navbar-brand">
                     <Link to='/' style={{textDecoration:'none',color:"white"}}>
                         <FontAwesomeIcon icon={faDev} /> 
                         <span className="resume-page-navbar-brand-my-name ms-2">Rohit Katbamna</span>
                     </Link>
-                </a>
+                </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
